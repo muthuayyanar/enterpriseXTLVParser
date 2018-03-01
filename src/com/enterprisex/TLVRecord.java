f82a908bf64fb8d0c9c0b6e5480d5f96941f3ee4
Thiru Chidambaram <thiru.chidambaram@appdynamics.com>
2018-02-28 22:31:07 -0800

    Added support for reading multiple records and cleand up most of the code..

---
+++ src/com/enterprisex/TLVRecord.java
@@ -0,0 +1,32 @@
+package com.enterprisex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TLVRecord {
+
+	private int _recordLength;
+	private List<TLVEntry> _entries;
+
+	public TLVRecord() {
+		_entries = new ArrayList<TLVEntry>();
+	}
+	
+	public int get_recordLength() {
+		return _recordLength;
+	}
+
+	public void set_recordLength(int _recordLength) {
+		this._recordLength = _recordLength;
+	}
+
+	public List<TLVEntry> get_entries() {
+		return _entries;
+	}
+
+	public void Add(TLVEntry entry) {
+		_entries.add(entry);
+	}
+	
+	
+}
