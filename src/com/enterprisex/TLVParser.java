30646a5a27c32b7b8b2b9eb29d533dd339abcfce
grbrazil <grbrazil@cisco.com>
2018-03-03 14:01:59 -0600

    update to make jar runnable

--- src/com/enterprisex/TLVParser.java
+++ src/com/enterprisex/TLVParser.java
@@ -1,10 +1,15 @@
 package com.enterprisex;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+
+import org.apache.commons.io.IOUtils;
+
 import java.nio.file.Path;
 
 public class TLVParser {
@@ -15,8 +20,15 @@ public class TLVParser {
 		System.out.println("Enterprise X - TLV Parser");
 		File tlv = new File("TLV/SCFFile.tlv");
 		Path path = Paths.get(tlv.getAbsolutePath());
+		
+		String resourceName = "SCFFile.tlv";
+		//ClassLoader classLoader = TLVParser.class.getResourceAsStream(resourceName);
+		//InputStream stream = classLoader.getResourceAsStream(resourceName);
+		InputStream stream = TLVParser.class.getResourceAsStream(resourceName);
+		
 		try {
-			byte[] data = Files.readAllBytes(path);
+			//byte[] data = Files.readAllBytes(path);
+			byte[] data = IOUtils.toByteArray(stream);
 			TLV test = new TLV();
 			TLVReader reader = new TLVReader(data, test);
 			reader.parseHeader();
